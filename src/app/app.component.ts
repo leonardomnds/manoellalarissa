2d1fcb49f5c6cfc124731ee24cc683423a68df21
Leonardo Mendes <leonardo.mendes@insidesistemas.com.br>
2024-06-10 13:44:01 -0300

    fix: remove alteração de meta description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, DestroyRef, inject } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { FaConfig } from '@fortawesome/angular-fontawesome';
+import { Component, inject } from '@angular/core';
 import { NgbDropdownConfig, NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
-import { filter, mergeMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +9,6 @@ import { filter, mergeMap } from 'rxjs';
 })
 export class AppComponent {
 
-  private router = inject(Router);
-  private document = inject(DOCUMENT);
-  private route = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
   private tooltipConfig = inject(NgbTooltipConfig);
   private dropdownConfig = inject(NgbDropdownConfig);
 
@@ -25,24 +16,6 @@ export class AppComponent {
     this.dropdownConfig.container = 'body';
     this.tooltipConfig.container = 'body';
     this.tooltipConfig.placement = ['bottom', 'top', 'bottom-right', 'bottom-left', 'top-right', 'top-left'];
-
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationEnd),
-        mergeMap(() => {
-          let currentRoute = this.route.root;
-          while (currentRoute.firstChild) { currentRoute = currentRoute.firstChild; }
-          return currentRoute.data;
-        }),
-        takeUntilDestroyed(this.destroyRef)
-      ).subscribe((routeData) => {
-        const metaDescription = routeData.metaDescription || DEFAULT_DESCRIPTION;
-
-        const metaTag = this.document.querySelector('meta[name="description"]');
-        if (metaTag) { metaTag.setAttribute('content', metaDescription); }
-      });
   }
 
 }
-
-const DEFAULT_DESCRIPTION = 'Descubra o design inovador e sustentável com Théa Prado Arquitetura. Especialistas em projetos arquitetônicos residenciais e comerciais, combinamos estética e funcionalidade para transformar espaços. Entre em contato e realize seu sonho com a nossa equipe experiente.'
