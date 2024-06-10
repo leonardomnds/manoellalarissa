d77132ed211557e4cf8f569a0ff9b766ac1974ff
Leonardo Mendes <leonardo.mendes@insidesistemas.com.br>
2024-06-09 23:56:18 -0300

    feat: layout

--- src/app/layout/content/header/header.component.ts
+++ src/app/layout/content/header/header.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, DestroyRef, ElementRef, ViewChild, ViewChildren, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  private document = inject(DOCUMENT);
+  private destroyRef = inject(DestroyRef);
+
+  @ViewChild('logo', { static: true })
+  private logoWrapper!: ElementRef<HTMLDivElement>;
+
+  constructor() {
+    interval(50)
+      .pipe(
+        map(() => this.logoWrapper.nativeElement),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((logoWrapper) => {
+        const deveExibir = this.document.documentElement.scrollTop >= 130;
+
+        if (deveExibir) {
+          logoWrapper.classList.add(LOGO_VISIBLE_CLASS);
+          return;
+        }
+
+        logoWrapper.classList.remove(LOGO_VISIBLE_CLASS);
+      });
+  }
+
+}
+
+const LOGO_VISIBLE_CLASS = 'visible'
