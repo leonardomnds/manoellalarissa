ef202f209d88bb82ab8b6bdd9fa6b885cfd845df
Leonardo Mendes <leonardo.mendes@insidesistemas.com.br>
2024-06-10 16:25:26 -0300

    fix: resolve ssg

--- src/app/layout/content/header/header.component.ts
+++ src/app/layout/content/header/header.component.ts
@@ -1,48 +1,27 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, DestroyRef, ElementRef, HostBinding, HostListener, TemplateRef, ViewChild, ViewChildren, inject } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AfterContentInit, Component, ElementRef, HostListener, TemplateRef, ViewChild, inject } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { NgbOffcanvas, NgbOffcanvasRef } from '@ng-bootstrap/ng-bootstrap';
-import { filter, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterContentInit {
 
   menuIcon = faBars;
 
   private sidebarRef?: NgbOffcanvasRef;
 
   private document = inject(DOCUMENT);
-  private destroyRef = inject(DestroyRef);
   private sidebarService = inject(NgbOffcanvas);
 
   @ViewChild('logo', { static: true })
   private logoWrapper!: ElementRef<HTMLDivElement>;
 
-  constructor() {
-    interval(50)
-      .pipe(
-        map(() => ({
-          logoWrapper: this.logoWrapper.nativeElement,
-          imageHeight: this.document.querySelector<HTMLImageElement>('section.initial .logo')?.height ?? 0
-        })),
-        filter(({ imageHeight }) => !!imageHeight),
-        takeUntilDestroyed(this.destroyRef)
-      )
-      .subscribe((data) => {
-        const deveExibir = this.document.documentElement.scrollTop >= data.imageHeight;
-
-        if (deveExibir) {
-          data.logoWrapper.classList.add(LOGO_VISIBLE_CLASS);
-          return;
-        }
-
-        data.logoWrapper.classList.remove(LOGO_VISIBLE_CLASS);
-      });
+  ngAfterContentInit(): void {
+    this.onWindowScroll();
   }
 
   abrirMenu(content: TemplateRef<any>): void {
@@ -55,6 +34,23 @@ export class HeaderComponent {
 
     this.sidebarRef.close();
     this.sidebarRef = undefined;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const logoWrapper = this.logoWrapper.nativeElement;
+    const imageHeight = this.document.querySelector<HTMLImageElement>('section.initial .logo')?.height ?? 0;
+
+    if (!imageHeight) { return; }
+
+    const deveExibir = this.document.documentElement.scrollTop >= imageHeight;
+
+    if (deveExibir) {
+      logoWrapper.classList.add(LOGO_VISIBLE_CLASS);
+      return;
+    }
+
+    logoWrapper.classList.remove(LOGO_VISIBLE_CLASS);
   }
 
 }
