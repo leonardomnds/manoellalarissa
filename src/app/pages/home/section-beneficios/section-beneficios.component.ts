c71521d1d9ae7f8f1e72518378d7dc04b3a36bc8
Leonardo Mendes <leonardo.mendes@insidesistemas.com.br>
2024-06-11 10:29:12 -0300

    feat: altera beneficios

--- src/app/pages/home/section-beneficios/section-beneficios.component.ts
+++ src/app/pages/home/section-beneficios/section-beneficios.component.ts
@@ -10,42 +10,42 @@ export class SectionBeneficiosComponent {
 
   beneficios: Beneficio[] = [
     {
-      titulo: 'Desenvolve relações saudáveis',
+      titulo: 'Desenvolver relações saudáveis',
       imagem: '01.webp',
       from: AnimationType.Left,
     },
     {
-      titulo: 'Aumenta a autoestima e autoconfiança',
+      titulo: 'Aumentar a autoestima e autoconfiança',
       imagem: '02.webp',
       from: AnimationType.Bottom,
     },
     {
-      titulo: 'Ensina a lidar com traumas',
+      titulo: 'Aprender a lidar com traumas',
       imagem: '03.webp',
       from: AnimationType.Bottom,
     },
     {
-      titulo: 'Compreende os pensamentos negativos',
+      titulo: 'Compreender os pensamentos negativos',
       imagem: '04.webp',
       from: AnimationType.Right,
     },
     {
-      titulo: 'Mudança de comportamentos',
+      titulo: 'Mudar de comportamentos',
       imagem: '05.webp',
       from: AnimationType.Bottom,
     },
     {
-      titulo: 'Compreendes padrões comportamentais',
+      titulo: 'Compreender padrões comportamentais',
       imagem: '06.webp',
       from: AnimationType.Left,
     },
     {
-      titulo: 'Promove o autoconhecimento',
+      titulo: 'Promover o autoconhecimento',
       imagem: '07.webp',
       from: AnimationType.Right,
     },
     {
-      titulo: 'Controla os sintoma de ansiedade',
+      titulo: 'Controlar os sintoma de ansiedade',
       imagem: '08.webp',
       from: AnimationType.Bottom,
     },
